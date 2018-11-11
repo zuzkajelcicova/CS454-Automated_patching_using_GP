3ae04d14d6adddefeddff12defc3691d2993fb36
BUNVERDENZ <bunverdenz@gmail.com>
2018-11-12 04:24:29 +0900

    Compile not done

--- src/GP/JavaResult.java
+++ src/GP/JavaResult.java
@@ -1,21 +1,25 @@
 package GP;
 
 import AST.ASTHandler;
+import General.Utils;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class JavaResult {
     private float fitness = 0;
 //    pass or fail
     private int result;
-    private ASTHandler astHandler;
+    private Utils utils;
     private String TARGET_CODE = "";
 
-    public JavaResult(String TARGET_CODE, ASTHandler astHandler) {
+    public JavaResult(String TARGET_CODE, Utils utils) {
         this.TARGET_CODE = TARGET_CODE;
-        this.astHandler = astHandler;
+        this.utils = utils;
+        fitness_function();
     }
 
     public float getFitness() {
@@ -26,21 +30,18 @@ public class JavaResult {
         return result;
     }
 
-    public ASTHandler getAstHandler() {
-        return astHandler;
-    }
-
-    public void run() {
-
+    private int run(TestCase testCase) {
+        //TODO: use testcase as argument for each compilation
         try {
             System.out.println("**********");
-            this.result = runProcess("javac -cp src src/" + this.TARGET_CODE);
+            int result = runProcess("javac -cp src src/" + this.TARGET_CODE);
             System.out.println("**********");
-
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        //TODO: should it be 0?
+        return 0;
     }
 
     private void printLines(String cmd, InputStream ins) throws Exception {
@@ -61,11 +62,49 @@ public class JavaResult {
         return pro.exitValue();
     }
 
-    public void fitness_function(ASTHandler modified_AST){
+    private void fitness_function(){
+        BigInteger bi1 = new BigInteger("18");
+        BigInteger bi2 = new BigInteger("24");
+        BigInteger bi3 = bi1.gcd(bi2);
 
+        // assume I have neg and pos test case
+        ArrayList<TestCase> negTest = new ArrayList<>();
+        ArrayList<TestCase> posTest = new ArrayList<>();
+        // mock up test case
+        int[] a = new int[]{42,56};
+        TestCase testCase1 = new TestCase(a,14);
+        int[] b = new int[]{461952,116298};
+        TestCase testCase2 = new TestCase(b,18);
+        negTest.add(testCase1);
+        negTest.add(testCase2);
+        int[] c = new int[]{12,0};
+        TestCase testCase3 = new TestCase(c,12);
+        int[] d = new int[]{0,0};
+        TestCase testCase4 = new TestCase(d,0);
+        posTest.add(testCase3);
+        posTest.add(testCase4);
 
-
-        this.fitness =
+        // save path(node) that were visited by neg and pos test case
+        int negFail = 0;
+        int posFail = 0;
+        for (TestCase testcase: negTest){
+            int output = run(testcase);
+            negFail = negFail + output;
+            if(output == 1){
+                this.result = utils.FAIL;
+            }
+        }
+        for (TestCase testcase: posTest){
+            int output = run(testcase);
+            posFail = posFail + output;
+            if(output == 1){
+                this.result = utils.FAIL;
+            }
+        }
+        int negPass = negTest.size() - negFail;
+        int posPass = posTest.size() - posFail;
+        //TODO: change formula for fitness function
+        this.fitness = (10*negPass) + (1*posPass);
 
     }
 }
