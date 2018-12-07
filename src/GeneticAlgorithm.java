86b1973e8965e2df21a4197a714dcb6ec16a45f3
Peter <pm>
2018-12-07 15:51:08 +0900

    fixing: merge from master

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -19,15 +19,17 @@ public class GeneticAlgorithm {
     private ASTHandler astHandler;
     private HashMap<Integer, NodePair> candidateSpace;
     private ArrayList<Integer> candidateList;
-    private ArrayList<Individual> initialPopulation;
+    private ArrayList<Patch> initialPopulation;
     private int solutionList;
     private int solutionCounter;
-    private int targetNode;
+    private ArrayList<Integer> targetNodes;
     private long startTime;
     private int posTestsNumber;
     private int negTestNumber;
+    private Random random;
 
     public GeneticAlgorithm(int populationSize, Utils utils, ASTHandler astHandler, long startTime) {
+        this.random = new Random();
         this.populationSize = populationSize;
         this.utils = utils;
         this.astHandler = astHandler;
@@ -36,34 +38,34 @@ public class GeneticAlgorithm {
         this.solutionCounter = 1;
         this.numberOfGenerations = 1;
         this.solutionList = 0;
+        this.targetNodes = new ArrayList<>();
 
         for (Map.Entry<Integer, NodePair> entry : astHandler.getFaultSpace().entrySet()) {
-            targetNode = entry.getKey();
-            break;
+            targetNodes.add(entry.getKey());
         }
-
         this.startTime = startTime;
     }
 
-    public ArrayList<Individual> initialize(int initialPopulationSize, ASTHandler astHandler) {
+    public ArrayList<Patch> initialize(int initialPopulationSize) {
         int[] possibleOperations = new int[]{0, 1, 2};
         for (Map.Entry<Integer, NodePair> entry : candidateSpace.entrySet()) {
             candidateList.add(entry.getKey());
         }
 
-        ArrayList<Individual> population = new ArrayList<>();
+        ArrayList<Patch> population = new ArrayList<>();
 
         for (int i = 0; i < initialPopulationSize; i++) {
-            Individual individual = new Individual();
+            Patch patch = new Patch();
             int operation = getRandom(possibleOperations);
+            int targetNode = targetNodes.get(random.nextInt(targetNodes.size()));
             if (operation == 0) {
-                individual.getAllPatches().add(new Patch(operation, -1, targetNode));
+                patch.getAllEdits().add(new Edit(operation, -1, targetNode));
             } else {
-                int index = new Random().nextInt(candidateList.size());
+                int index = random.nextInt(candidateList.size());
                 int sourceNode = candidateList.get(index);
-                individual.getAllPatches().add(new Patch(operation, sourceNode, targetNode));
+                patch.getAllEdits().add(new Edit(operation, sourceNode, targetNode));
             }
-            population.add(individual);
+            population.add(patch);
         }
         return population;
     }
@@ -89,16 +91,16 @@ public class GeneticAlgorithm {
     public void repairProgram() {
         System.out.println("Running defect fixing...");
 
-        GeneticOperations gp = new GeneticOperations(utils, targetNode, candidateList);
+        GeneticOperations gp = new GeneticOperations(utils, targetNodes, candidateList);
 
         //Initialize population - initialize with more to have enough to choose from if a lot does not compile
-        this.initialPopulation = initialize(this.populationSize + populationSize / 2, this.astHandler);
+        this.initialPopulation = initialize(this.populationSize + populationSize / 2);
 
-        Individual fittestIndividual;
-        List<Individual> oldPopulation = new ArrayList<>();
-        List<Individual> newPopulation = new ArrayList<>();
-        List<Individual> tournamentSelectionParents;
-        List<Individual> offsprings;
+        Patch fittestPatch;
+        List<Patch> oldPopulation = new ArrayList<>();
+        List<Patch> newPopulation;
+        List<Patch> tournamentSelectionParents;
+        List<Patch> offsprings;
 
         //Initial population
         initialPopulation = compileAndTest(initialPopulation);
@@ -118,7 +120,7 @@ public class GeneticAlgorithm {
             this.populationSize = oldPopulation.size();
         }
 
-        fittestIndividual = gp.getFittest(oldPopulation);
+        fittestPatch = gp.getFittest(oldPopulation);
         newPopulation = populateList(oldPopulation);
 
         while (solutionList == 0) {
@@ -128,24 +130,28 @@ public class GeneticAlgorithm {
 
             //Crossovers and mutations
             offsprings = gp.crossover(tournamentSelectionParents);
-            offsprings = gp.mutate(offsprings, fittestIndividual);
+            offsprings = gp.mutate(offsprings, fittestPatch);
 
             offsprings = compileAndTest(offsprings);
+            if (solutionList > 0) {
+                return;
+            }
+
             offsprings = selectPopulationIndividuals(oldPopulation, offsprings);
 
             oldPopulation = populateList(newPopulation);
             //Create new population
             newPopulation = populateList(tournamentSelectionParents);
 
-            for (Individual individual : populateList(offsprings)) {
-                newPopulation.add(individual);
+            for (Patch patch : populateList(offsprings)) {
+                newPopulation.add(patch);
             }
         }
     }
 
-    private List<Individual> selectPopulationIndividuals(List<Individual> oldPopulation, List<Individual> offspringPopulation) {
+    private List<Patch> selectPopulationIndividuals(List<Patch> oldPopulation, List<Patch> offspringPopulation) {
         int threshold = populationSize / 2;
-        List<Individual> result = new ArrayList<>();
+        List<Patch> result = new ArrayList<>();
 
         if (offspringPopulation.size() == threshold) {
             return offspringPopulation;
@@ -172,13 +178,12 @@ public class GeneticAlgorithm {
         return null;
     }
 
-    private ArrayList<Individual> compileAndTest(List<Individual> newPopulation) {
-        ArrayList<Individual> candidateResult = new ArrayList<>();
+    private ArrayList<Patch> compileAndTest(List<Patch> newPopulation) {
+        ArrayList<Patch> candidateResult = new ArrayList<>();
 
-        for (Individual individual : newPopulation) {
-            astHandler.applyPatches(individual);
+        for (Patch patch : newPopulation) {
+            astHandler.applyPatches(patch);
 
-            //StringBuilder reformattedCode = utils.removeCodeLines();
             StringBuilder codeWithLines = utils.getCodeWithLines();
             utils.saveData(utils.SRC_DIRECTORY, utils.TARGET_CODE, codeWithLines);
 
@@ -199,19 +204,19 @@ public class GeneticAlgorithm {
 
                     double fitness = (utils.WEIGHT_NEG * negPass) + (utils.WEIGHT_POS * posPass);
                     System.out.println("Fitness Value: " + fitness);
-                    individual.setFitness(fitness);
-                    candidateResult.add(individual);
+                    patch.setFitness(fitness);
+                    candidateResult.add(patch);
 
-                    //We do not finish after finding the very first solution - full generation is finished
-                    //to see how many potential patches are available
+                    //Break after the first fixing Patch is found
                     if (testNegResult.getFailureCount() == 0 && testPosResult.getFailureCount() == 0) {
                         //Storing 2 files -> .java and .txt for statistics, Time in seconds
                         long requiredPatchTime = (System.currentTimeMillis() - startTime) / 1000;
-                        storeCodeAndStatistics(individual, codeWithLines, requiredPatchTime);
+                        storeCodeAndStatistics(patch, codeWithLines, requiredPatchTime);
                         this.solutionList++;
+                        break;
                     }
                 } else {
-                    individual.setFitness(0);
+                    patch.setFitness(0);
                 }
 
             } catch (Exception e) {
@@ -231,109 +236,13 @@ public class GeneticAlgorithm {
                     this.negTestNumber = GCDTestNeg.numberOfNegativeTests;
                     return GCDTestNeg.class;
                 }
-//            case "median.java":
-////                if (testType.equalsIgnoreCase(utils.positive)) {
-////                    this.posTestsNumber = median_PosTest.numberOfPositiveTests;
-////                    return median_PosTest.class;
-////                } else {
-////                    this.negTestNumber = median_NegTest.numberOfNegativeTests;
-////                    return median_NegTest.class;
-////                }
-            case "Triangle.java":
+            case "Zune.java":
                 if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = TestTriPos.numberOfPositiveTests;
-                    return TestTriPos.class;
+                    this.posTestsNumber = ZuneTestPos.numberOfPositiveTests;
+                    return GCDTestPos.class;
                 } else {
-                    this.negTestNumber = TestTriNeg.numberOfNegativeTests;
-                    return TestTriNeg.class;
-                }
-            case "Digits00.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits00PosTest.numberOfPositiveTests;
-                    return Digits00PosTest.class;
-                } else {
-                    this.negTestNumber = Digits00NegTest.numberOfNegativeTests;
-                    return Digits00NegTest.class;
-                }
-            case "Digits002.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits002PosTest.numberOfPositiveTests;
-                    return Digits002PosTest.class;
-                } else {
-                    this.negTestNumber = Digits002NegTest.numberOfNegativeTests;
-                    return Digits002NegTest.class;
-                }
-            case "Digits5000.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits5000PosTest.numberOfPositiveTests;
-                    return Digits5000PosTest.class;
-                } else {
-                    this.negTestNumber = Digits5000NegTest.numberOfNegativeTests;
-                    return Digits5000NegTest.class;
-                }
-            case "Digits5003.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits5003PosTest.numberOfPositiveTests;
-                    return Digits5003PosTest.class;
-                } else {
-                    this.negTestNumber = Digits5003NegTest.numberOfNegativeTests;
-                    return Digits5003NegTest.class;
-                }
-            case "Digits6000.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits6000PosTest.numberOfPositiveTests;
-                    return Digits6000PosTest.class;
-                } else {
-                    this.negTestNumber = Digits6000NegTest.numberOfNegativeTests;
-                    return Digits6000NegTest.class;
-                }
-            case "Digits10000.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits10000PosTest.numberOfPositiveTests;
-                    return Digits10000PosTest.class;
-                } else {
-                    this.negTestNumber = Digits10000NegTest.numberOfNegativeTests;
-                    return Digits10000NegTest.class;
-                }
-            case "Digits11004.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits11004PosTest.numberOfPositiveTests;
-                    return Digits11004PosTest.class;
-                } else {
-                    this.negTestNumber = Digits11004NegTest.numberOfNegativeTests;
-                    return Digits11004NegTest.class;
-                }
-            case "Digits11015.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits11015PosTest.numberOfPositiveTests;
-                    return Digits11015PosTest.class;
-                } else {
-                    this.negTestNumber = Digits11015NegTest.numberOfNegativeTests;
-                    return Digits11015NegTest.class;
-                }
-            case "Digits12000.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits11015PosTest.numberOfPositiveTests;
-                    return Digits11015PosTest.class;
-                } else {
-                    this.negTestNumber = Digits11015NegTest.numberOfNegativeTests;
-                    return Digits11015NegTest.class;
-                }
-            case "Digits39000.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Digits39000PosTest.numberOfPositiveTests;
-                    return Digits39000PosTest.class;
-                } else {
-                    this.negTestNumber = Digits39000NegTest.numberOfNegativeTests;
-                    return Digits39000NegTest.class;
-                }
-            case "Smallest19.java":
-                if (testType.equalsIgnoreCase(utils.positive)) {
-                    this.posTestsNumber = Smallest19PosTest.numberOfPositiveTests;
-                    return Smallest19PosTest.class;
-                } else {
-                    this.negTestNumber = Smallest19NegTest.numberOfNegativeTests;
-                    return Smallest19NegTest.class;
+                    this.negTestNumber = ZuneTestNeg.numberOfNegativeTests;
+                    return GCDTestNeg.class;
                 }
             default:
                 return null;
@@ -350,32 +259,29 @@ public class GeneticAlgorithm {
         System.out.println("Run Time: " + testResult.getRunTime());
     }
 
-    private void storeCodeAndStatistics(Individual individual, StringBuilder reformattedCode, long requiredPatchTime) {
+    private void storeCodeAndStatistics(Patch patch, StringBuilder reformattedCode, long requiredPatchTime) {
         String javaFile = utils.FIXED_JAVA + solutionCounter + ".java";
         String statistics = utils.FIXED_JAVA_STATISTICS + solutionCounter + ".txt";
 
-        StringBuilder individualStatistics = new StringBuilder();
-        individualStatistics.append("Target: ").append(astHandler.getFaultSpace().get(targetNode).getNode().getTextContent()).append(utils.LINE_SEPARATOR);
+        StringBuilder individualStatistics;
 
-        individualStatistics.append("Sources: to be added");
-
-        individualStatistics = individual.getAllPatchesContent();
+        individualStatistics = patch.getAllPatchesContent();
         individualStatistics.append(utils.LINE_SEPARATOR).append(utils.LINE_SEPARATOR);
 
         individualStatistics.append("Time: " + requiredPatchTime).append(" seconds").append(utils.LINE_SEPARATOR);
         individualStatistics.append("Generation: " + numberOfGenerations).append(utils.LINE_SEPARATOR);
-        individualStatistics.append("Mutations: " + individual.getCtrMutation()).append(utils.LINE_SEPARATOR);
-        individualStatistics.append("Crossovers: " + individual.getCtrCrossover()).append(utils.LINE_SEPARATOR);
+        individualStatistics.append("Mutations: " + patch.getCtrMutation()).append(utils.LINE_SEPARATOR);
+        individualStatistics.append("Crossovers: " + patch.getCtrCrossover()).append(utils.LINE_SEPARATOR);
 
         utils.saveData(utils.SOLUTION_DIRECTORY, javaFile, reformattedCode);
         utils.saveData(utils.SOLUTION_DIRECTORY, statistics, individualStatistics);
         solutionCounter++;
     }
 
-    private List<Individual> populateList(List<Individual> sourceList) {
-        List<Individual> targetList = new ArrayList<>();
-        for (Individual currentIndividual : sourceList) {
-            targetList.add(currentIndividual);
+    private List<Patch> populateList(List<Patch> sourceList) {
+        List<Patch> targetList = new ArrayList<>();
+        for (Patch currentPatch : sourceList) {
+            targetList.add(currentPatch);
         }
         return targetList;
     }
