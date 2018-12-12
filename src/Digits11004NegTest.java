36412ae49086f9682cbec8c5a317e018aa385583
Peter <pm>
2018-12-13 01:35:16 +0900

    added tests

---
+++ src/Digits11004NegTest.java
@@ -0,0 +1,111 @@
+import junit.framework.TestResult;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static java.lang.System.out;
+
+public class Digits11004NegTest extends TestResult {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(5);
+    public static int numberOfNegativeTests = 2;
+
+    public static Class testClass = null;
+    public static Method[] allMethods = null;
+    public static Method testMethod = null;
+    public static String testedMethodName = "exec";
+
+    @BeforeClass
+    public static void setupEnvironment() {
+        //Get access to a recompiled class during the runtime
+        testClass = Digits11004.class;
+        allMethods = CompiledClassLoader.getRecompiledMethods(testClass);
+
+        for (Method m : allMethods) {
+            String methodName = m.getName();
+            if (methodName.startsWith(testedMethodName)) {
+                m.setAccessible(true);
+                testMethod = m;
+            }
+        }
+    }
+    @Test
+    public void test1() {
+        try {
+            out.format("Invoking %s()%n", testedMethodName, " from test1...");
+            String expected = "Enter an integer >  2 6 6 8 6 5 5 0 0 1 That's all, have a nice day!";
+            Object o = testMethod.invoke(null, 1005568662);
+            String out = o.toString().replace("\n", "").trim();
+            Assert.assertEquals(expected.replace(" ", ""),
+                    out.replace(" ", ""));
+            out.format("%s() returned %b%n", testedMethodName, o);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2() {
+        try {
+            out.format("Invoking %s()%n", testedMethodName, " from test1...");
+            String expected = "Enter an integer >  6 7 8 -9 That's all, have a nice day!";
+            Object o = testMethod.invoke(null, -9876);
+            String out = o.toString().replace("\n", "").trim();
+            Assert.assertEquals(expected.replace(" ", ""),
+                    out.replace(" ", ""));
+            out.format("%s() returned %b%n", testedMethodName, o);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Test (timeout = 1000) public void test3 () throws Exception {
+//        Digits00 mainClass = new Digits00();
+//        String expected =
+//            "Enter an integer >  2 7 2 0 3 7 3 That's all, have a nice day!";
+//        mainClass.scanner = new java.util.Scanner ("3730272");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test4 () throws Exception {
+//        Digits00 mainClass = new Digits00();
+//        String expected = "Enter an integer >  8 That's all, have a nice day!";
+//        mainClass.scanner = new java.util.Scanner ("8");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test5 () throws Exception {
+//        Digits00 mainClass = new Digits00();
+//        String expected =
+//            "Enter an integer >  4 7 3 9 8 That's all, have a nice day!";
+//        mainClass.scanner = new java.util.Scanner ("89374");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test6 () throws Exception {
+//        Digits00 mainClass = new Digits00();
+//        String expected =
+//            "Enter an integer >  6 6 6 6 6 6 6 That's all, have a nice day!";
+//        mainClass.scanner = new java.util.Scanner ("6666666");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+}
---
+++ src/Smallest19PosTest.java
@@ -0,0 +1,134 @@
+import junit.framework.TestResult;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+public class Smallest19PosTest extends TestResult {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(5);
+    public static int numberOfPositiveTests = 2;
+
+    public static Class testClass = null;
+    public static Method[] allMethods = null;
+    public static Method testMethod = null;
+    public static String testedMethodName = "exec";
+
+    @BeforeClass
+    public static void setupEnvironment() {
+        //Get access to a recompiled class during the runtime
+        testClass = Smallest19.class;
+        allMethods = CompiledClassLoader.getRecompiledMethods(testClass);
+
+        for (Method m : allMethods) {
+            String methodName = m.getName();
+            if (methodName.startsWith(testedMethodName)) {
+                m.setAccessible(true);
+                testMethod = m;
+            }
+        }
+    }
+
+    @Test
+    public void test1() {
+        try {
+            String expected = "Please enter 4 numbers separated by spaces > 1 is the smallest";
+            Object o = testMethod.invoke(null, 1, 2, 3, 4);
+            Assert.assertEquals(expected.replace(" ", ""),
+                    o.toString().replace(" ", "").trim());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2() {
+        try {
+            String expected = "Please enter 4 numbers separated by spaces > 1 is the smallest";
+            Object o = testMethod.invoke(null, 4, 3, 2, 1);
+            Assert.assertEquals(expected.replace(" ", ""),
+                    o.toString().replace(" ", "").trim());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Test (timeout = 1000) public void test2 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > 1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("4 3 2 1");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test3 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > 1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("3 4 2 1");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test4 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > 1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("3 2 4 1");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test5 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > 1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("1 1 1 1");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test6 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > 2 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("2 2 2 3");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test7 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > -1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("0 0 0 -1");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+//    @Test (timeout = 1000) public void test8 () throws Exception {
+//        Smallest19 mainClass = new Smallest19 ();
+//        String expected =
+//            "Please enter 4 numbers separated by spaces > -1 is the smallest";
+//        mainClass.scanner = new java.util.Scanner ("0 -1 0 0");
+//        mainClass.exec ();
+//        String out = mainClass.output.replace ("\n", " ").trim ();
+//        assertEquals (expected.replace (" ", ""), out.replace (" ", ""));
+//    }
+}
